d82fef5096e6086b057daddb45fab95d282206b3
varuntirumala1 <varun.tirumala@gmail.com>
2021-02-20 22:52:07 -0500

    Create healthcheck.sh

---
+++ healthcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+serv=cloudflared
+sstat=stopped
+service $serv status | grep -i 'running\|stopped' | awk '{print $3}' | while read output;
+do
+echo $output
+if [ "$output" == "$sstat" ]; then
+    service $serv start
+    echo "$serv service is UP now.!"
+    else
+    echo "$serv service is running"
+    fi
+done
